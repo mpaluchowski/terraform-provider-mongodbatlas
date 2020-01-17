75f3ddb7918dd28233ac952224740e26f8be86e2
PacoDw <__pm@outlook.com>
2020-01-17 12:55:24 -0600

    chore: it was removed it due to unexpected issue it didn't work fine

--- mongodbatlas/resource_mongodbatlas_custom_db_role.go
+++ mongodbatlas/resource_mongodbatlas_custom_db_role.go
@@ -36,9 +36,9 @@ func resourceMongoDBAtlasCustomDBRole() *schema.Resource {
 				ForceNew: true,
 				ValidateFunc: validation.All(
 					validation.StringMatch(regexp.MustCompile(`[\w\d-]+`), "`role_name` can contain only letters, digits, underscores, and dashes"),
-					validation.StringMatch(regexp.MustCompile(`^atlasAdmin$`), "`role_name` cannot be 'atlasAdmin'"),
 					func(v interface{}, k string) (ws []string, es []error) {
 						value := v.(string)
+						log.Printf("LOG___ value: %#+v\n", value)
 						if strings.HasPrefix(value, "x-gen") {
 							es = append(es, fmt.Errorf("`role_name` cannot start with 'xgen-'"))
 						}
